980003a7291066171251d64b49601a442f3ee7e0
Houston <sulima.anton2013@yandex.ru>
2021-08-16 23:52:45 +0300

    Implemented random cells

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 "use strict;";
 
-function fillTable(table, width, height) {
-    for (let i = 0; i < height; i++) {
-        let tempRow = `<tr id='${i}'>`;
-        for (let k = 0; k < width; k++) {
-            tempRow += `<td id='${k.toString() + i.toString()}'></td>`;
+class PlayField {
+    constructor(TableBodySelector, Width, Height, RefreshTime, MarkClassName) {
+        this.TableBody = document.querySelector(TableBodySelector);
+        this.Width = Width;
+        this.Height = Height;
+        this.RefreshTime = RefreshTime;
+        this.MarkClassName = MarkClassName;
+    }
+
+    CreateTable() {
+        for (let i = 0; i < this.Height; i++) {
+            let tempRow = `<tr>`;
+            for (let k = 0; k < this.Width; k++) {
+                tempRow += `<td id='${k.toString() + '-' + i.toString()}'></td>`;
+            }
+            tempRow += "</tr>";
+            this.TableBody.innerHTML += tempRow;
         }
-        tempRow += "</tr>";
-        table.innerHTML += tempRow;
     }
-}
-
-function RandomLife(width, height) {
-    let i = 0;
-    while (i < 100) {
+    SimulateRandomLife() {
+        this.intervalID = setInterval(() => {
+            const P = new Promise((resolve, reject) => {
+                setTimeout(() => { this.ClearCells(); resolve(); }, this.RefreshTime);
+            }).then(() => {
+                this.RandomLife();
+            });
+        }, this.RefreshTime * 2);
+    }
+    RandomLife() {
+        let i = 0;
+        while (i < this.Width * this.Height * 0.2) {
+            let id = Math.floor(Math.random() * this.Height).toString() + '-' + Math.floor(Math.random() * this.Width).toString();
+            let element = document.getElementById(id);
+            if (!element.classList.contains(this.MarkClassName))
+                element.classList += this.MarkClassName;
+            else 
+                continue;
+            i++;
+        }
+    }
+    ClearCells() { 
+        document.querySelectorAll('.' + this.MarkClassName).forEach(element => element.classList.remove(this.MarkClassName));
+    }
+    SimulateLife() {
         
-        let id = Math.floor(Math.random() * height).toString() + Math.floor(Math.random() * width).toString();
-        let element = document.getElementById(id);
-        element.classList += "alive";
-        i++;
     }
+ 
 }
 
-const tableBody = document.querySelector("tbody");
-fillTable(tableBody, 20, 20);
-RandomLife(10, 10);
+
+const Field = new PlayField("tbody", 20, 20, 35, "alive");
+Field.CreateTable();
+Field.SimulateRandomLife();
+
+
+
+
+
+
